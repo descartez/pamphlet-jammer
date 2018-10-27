0a829917f0706a562453b481e2fe8bc62b1be0aa
Ryan Stong <rvanstong@gmail.com>
2018-10-27 00:15:51 -0700

    adds in config file for handling of essid and auth and password

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import network
+import ujson
+import usocket as socket
+
+# opens up config file
+f = open("config.json", "r")
+configs = f.read()
+config = ujson.loads(configs)
+print(config)
+f.close()
+
+# sets up local network using config.json
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='Free Resources')
-ap.config(authmode=3, password='123456789')
+ap.config(essid=config['essid'], authmode=config['authtype'])
+
+if config['authtype'] != 0:
+    ap.config(password=config['essid_password'])
